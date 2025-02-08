7d4aa961b79294e6fd2c51270f912349af74a153
Marc-Aurèle Brothier <m@brothier.org>
2025-02-09 00:02:17 +0100

    fix: incorrect use of for-else

--- metrics/main.py
+++ metrics/main.py
@@ -8,6 +8,18 @@ app = FastAPI()
 ipx_host = os.getenv("IPX_API_URL")
 ipx = ipx800(ipx_host, os.getenv("IPX_API_KEY"))
 
+relays = []
+analogs = []
+counters = []
+all = False if os.getenv("READ", "") else True
+for value in os.getenv("READ", "").split(","):
+    if value.startswith("A"):
+        analogs.append(value.split("."))
+    elif value.startswith("R"):
+        relays.append(value)
+    elif value.startswith("C"):
+        counters.append(value)
+
 
 @app.get("/metrics")
 def gce_ipx800_metrics():
@@ -18,28 +30,15 @@ def gce_ipx800_metrics():
     `READ=R6,R1,A1.as_tc4012,A2.value,C1`
     If READ is not provided it will return all values.
     """
-    relays = []
-    analogs = []
-    counters = []
-    all = False
-    for value in os.getenv("READ", "").split(","):
-        if value.startswith("A"):
-            analogs.append(value.split("."))
-        elif value.startswith("R"):
-            relays.append(value)
-        elif value.startswith("C"):
-            counters.append(value)
-    else:
-        all = True
-
     data = ""
     if analogs or all:
         data += "# TYPE gce_ipx800_analogs gauge\n"
         data += "# HELP gce_ipx800_analogs Value of the analog sensors\n"
-        for a in analogs:
-            idx = int(a[0][1:])
-            attribute = a[1]
-            data += f'gce_ipx800_analogs{{host="{ipx_host}", analog="{a[0]}"}} {round(getattr(ipx.analogs[idx - 1], attribute), 3)}\n'
+        if analogs:
+            for a in analogs:
+                idx = int(a[0][1:])
+                attribute = a[1]
+                data += f'gce_ipx800_analogs{{host="{ipx_host}", analog="{a[0]}"}} {round(getattr(ipx.analogs[idx - 1], attribute), 3)}\n'
         else:
             for index, val in enumerate(ipx.analogs):
                 data += f'gce_ipx800_analogs{{host="{ipx_host}", analog="A{index + 1}"}} {round(val.value, 3)}\n'
@@ -47,9 +46,10 @@ def gce_ipx800_metrics():
     if counters or all:
         data += "# TYPE gce_ipx800_counters_total counter\n"
         data += "# HELP gce_ipx800_counters_total Value of the counters\n"
-        for c in counters:
-            idx = int(c[1:])
-            data += f'gce_ipx800_counters_total{{host="{ipx_host}", counter="{c}"}} {ipx.counters[idx - 1].value}\n'
+        if counters:
+            for c in counters:
+                idx = int(c[1:])
+                data += f'gce_ipx800_counters_total{{host="{ipx_host}", counter="{c}"}} {ipx.counters[idx - 1].value}\n'
         else:
             for index, val in enumerate(ipx.counters):
                 data += f'gce_ipx800_counters_total{{host="{ipx_host}", counter="C{index + 1}"}} {val.value}\n'
@@ -59,9 +59,10 @@ def gce_ipx800_metrics():
         data += (
             "# HELP gce_ipx800_relays_state Status of the relay, either 0=OFF or 1=ON\n"
         )
-        for r in relays:
-            idx = int(r[1:])
-            data += f'gce_ipx800_relays_state{{host="{ipx_host}", relay="{r}"}} {int(ipx.relays[idx - 1].status)}\n'
+        if relays:
+            for r in relays:
+                idx = int(r[1:])
+                data += f'gce_ipx800_relays_state{{host="{ipx_host}", relay="{r}"}} {int(ipx.relays[idx - 1].status)}\n'
         else:
             for index, val in enumerate(ipx.relays):
                 data += f'gce_ipx800_relays_state{{host="{ipx_host}", relay="R{index + 1}"}} {int(val.status)}\n'
